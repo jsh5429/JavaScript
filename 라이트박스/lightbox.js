ba8a7a1168a5e984f72e25dd63743c7b4ae3636f
SH J <thdghk5429@gmail.com>
2023-03-17 18:06:06 +0900

    0317 lightbox

--- 라이트박스/lightbox.js
+++ 라이트박스/lightbox.js
@@ -19,13 +19,20 @@ for(let i=0; i<pics.length; i++){
 function showLightbox(e){
     // 이미지수정
     // data-이름 작성한 값은 dataset을 통해 접근
-    let img = e.target.dataset.src;
+    // 이벤트 대상, 즉 클릭한 그림의 data-src로 접근해
+    // data-src안의 값, 즉 이미지의 경로 정보를 가져와
+    // 변수 img에 넣는다.
+    const imgTag = e.target;
+    let img = imgTag.dataset.src;
 
     // 이미지 값을 가져와서 lightbox의 이미지에 추가
     // 이렇게 작성해야 클릭한 이미지가 라이트 박스에 추가되어
     // 그대로 커진 이미지를 보여준다.
+    // lightbox의 첫 요소. img태그의 src속성에 
+    // 가져온 img정보를 넣는다.
+    // 이벤트가 발생한 이미지를 lightbox에 할당.
     lightbox.firstElementChild.src = img;
-    //라이트 박스 보임
+    //라이트 박스 보이게 하는 코드.
     lightbox.style.display = "block";
 }
 
